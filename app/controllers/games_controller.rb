b1961e45835e7c14158c3f39eb624c66e300cd25
Jasmine Dang <ngandth.1992@gmail.com>
2020-05-09 21:09:37 +0800

    Added validity logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,41 @@
+require 'open-uri'
+require 'json'
 class GamesController < ApplicationController
   def new
     @letters = []
     10.times do
       @letters << ((rand * 25).round + 65).chr
     end
+    session[:letters] = @letters
   end
+
   def score
+    @answer = params[:answer]
+    @letters = params[:letters]
+
+    dict_url = "https://wagon-dictionary.herokuapp.com/#{@answer}"
+    @word_data = JSON.parse(open(dict_url).read)
+
+    @result = ''
+    if valid_word?(@word_data) && given_letters?(@answer, @letters)
+      @result = "Congratulations! #{@answer.upcase} is a valid word."
+    elsif !given_letters?(@answer, @letters)
+      @result = "Sorry but #{@answer.upcase} can't be built out of #{@letters}."
+    elsif !valid_word?(@word_data)
+      @result = "Sorry but #{@answer.upcase} does not seem to be a valid English
+       word."
+    end
+    @test = session[:letters]
+  end
+
+  private
+
+  def valid_word?(hash)
+    hash['found'] == true
+  end
+
+  def given_letters?(word, array)
+    letters = word.split(//)
+    letters.all? { |letter| array.include?(letter.upcase) }
   end
 end
